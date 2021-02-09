28bfa8bfcffe743088c74a429ba0e1c65bfa6a0e
danpros <dan@danpros.com>
2021-02-09 22:19:11 +0700

    Update static.html.php

--- static.html.php
+++ static.html.php
@@ -1,7 +1,7 @@
 <article class="post-full post">
 	<header class="post-full-header">
 		<h1 class="post-full-title"><?php echo $p->title;?></h1>
-		<?php if (login()) { echo tab($p); } ?>
+		<?php if (login()):?><a href="<?php echo $p->url;?>/edit?destination=post">Edit</a><?php endif;?>
 	</header>
 
 	<section class="post-full-content">
